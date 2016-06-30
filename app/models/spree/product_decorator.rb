b73aa5ee307ad90f3ca30144ab70757f069615f3
Tony Fontenot <tony.fontenot@spokecustom.com>
2016-06-30 15:30:28 -0400

    handle tiers through base product

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,4 +2,12 @@ Spree::Product.class_eval do
   def price_for_user(user)
     self.master.price_for_user(user)
   end
+  
+  def has_pricing_tiers?(user = Spree::User.current)
+    user.try(:user_group).try(:has_pricing_tiers_for_variant?, self) || false
+  end
+  
+  def pricing_tiers(user = Spree::User.current)
+    user.try(:user_group).try(:pricing_tiers_for_variant, self) || []
+  end
 end
